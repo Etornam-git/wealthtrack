53585b6757ddd81e3c4a672e88ef28bdc556e57b
Etornam-git <lawrenceagbogedenu76@gmail.com>
2025-04-03 02:25:18 +0000

    Relationship between accounts, transactions and users

--- database/migrations/2025_03_31_232005_create_transactions_table.php
+++ database/migrations/2025_03_31_232005_create_transactions_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\User;
+use App\Models\Account;
 
 return new class extends Migration
 {
@@ -14,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Account::class)->constrained()->cascadeOnDelete();
             $table->Integer('amount');
             $table->String('transaction_type');
             $table->String('description');
